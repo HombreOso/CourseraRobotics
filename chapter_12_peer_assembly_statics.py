33bda69395ad245a031403fb7f7c5816aed1af68
HombreOso <arthur.erich.emig@gmail.com>
2026-05-20 09:32:10 +0200

    the last peer-graded assignment for tthe chapter 12 initialized

---
+++ chapter_12_peer_assembly_statics.py
@@ -0,0 +1,9 @@
+import csv
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import modern_robotics as mr
+from scipy.optimize import linprog
+from dataclasses import dataclass
+
